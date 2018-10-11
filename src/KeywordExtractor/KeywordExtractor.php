a72d5f9c06ea2d01163ed972f459cf1041d13cd4
Ehsan <iranianpep@gmail.com>
2018-10-12 00:27:19 +1100

    changes

--- src/KeywordExtractor/KeywordExtractor.php
+++ src/KeywordExtractor/KeywordExtractor.php
@@ -4,31 +4,38 @@ namespace KeywordExtractor;
 
 class KeywordExtractor
 {
-    public function hello(): string
-    {
-        return 'hi';
-    }
-
-    public function generateNgram($tokens, $ngram = 2)
+    public function generateNgram(array $tokens, $ngram = 2): array
     {
         $ngrams = [];
-        for ($i = 0; $i <= count($tokens); $i = $i + ($ngram - 1)) {
+        $length = count($tokens) - $ngram + 1;
+        for ($i = 0; $i < $length; $i++) {
+            $slice = array_slice($tokens, $i, $ngram);
 
-            $text = '';
-            for ($j = $i - ($ngram - 1); $j < $i + ($ngram - 1); $j++) {
-                if (!isset($tokens[$j])) {
-                    $text = '';
-                    break;
-                }
-
-                $text .= $tokens[$j] . ' ';
-            }
-
-            if (!empty($text)) {
-                $ngrams[] = trim($text);
-            }
+            $ngrams[$i] = implode(' ', $slice);
         }
 
+        return $ngrams;
+    }
+
+    public function generateNgram2(array $tokens, $nGramSize, $separator = ' ') : array
+    {
+        $separatorLength = strlen($separator);
+        $length = count($tokens) - $nGramSize + 1;
+        if($length < 1) {
+            return [];
+        }
+        $ngrams = array_fill(0, $length, ''); // initialize the array
+
+        for($index = 0; $index < $length; $index++)
+        {
+            for($jindex = 0; $jindex < $nGramSize; $jindex++)
+            {
+                $ngrams[$index] .= $tokens[$index + $jindex];
+                if($jindex < $nGramSize - $separatorLength) {
+                    $ngrams[$index] .= $separator;
+                }
+            }
+        }
         return $ngrams;
     }
 }
--- tests/KeywordExtractor/KeywordExtractorTest.php
+++ tests/KeywordExtractor/KeywordExtractorTest.php
@@ -6,33 +6,91 @@ use PHPUnit\Framework\TestCase;
 
 class KeywordExtractorTest extends TestCase
 {
-    public function testHello()
-    {
-        $greeting = new KeywordExtractor();
-
-        $this->assertEquals('hi', $greeting->hello());
-    }
-
     public function testGenerateNgram()
     {
         $greeting = new KeywordExtractor();
-        $input = [
-            'this',
-            'is',
-            'an',
-            'example'
-        ];
+        $input = ['this', 'is', 'an', 'example'];
 
-//        $ngrams = $greeting->generateNgram($input, 1);
-//        $this->assertEquals($input, $ngrams);
+        $ngrams = $greeting->generateNgram($input, 1);
+        $this->assertEquals($input, $ngrams);
 
-        $expected = [
-            'this is',
-            'is an',
-            'an example'
-        ];
+        $expected = ['this is', 'is an', 'an example'];
 
         $ngrams = $greeting->generateNgram($input, 2);
         $this->assertEquals($expected, $ngrams);
+
+        $expected = ['this is an', 'is an example'];
+
+        $ngrams = $greeting->generateNgram($input, 3);
+        $this->assertEquals($expected, $ngrams);
+
+        $expected = ['this is an example'];
+
+        $ngrams = $greeting->generateNgram($input, 4);
+        $this->assertEquals($expected, $ngrams);
+
+        $input = ['this', 'is'];
+
+        $ngrams = $greeting->generateNgram($input, 4);
+        $this->assertEquals([], $ngrams);
+
+        $bigText = "A more principled way to estimate sentence importance is using random walks 
+and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
+to TextRank, and both use this approach for document summarization. The two 
+methods were developed by different groups at the same time, and LexRank 
+simply focused on summarization, but could just as easily be used for
+keyphrase extraction or any other NLP ranking task. A more principled way to 
+estimate sentence importance is using random walks 
+and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
+to TextRank, and both use this approach for document summarization. The two 
+methods were developed by different groups at the same time, and LexRank 
+simply focused on summarization, but could just as easily be used for
+keyphrase extraction or any other NLP ranking task. A more principled way to 
+estimate sentence importance is using random walks 
+and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
+to TextRank, and both use this approach for document summarization. The two 
+methods were developed by different groups at the same time, and LexRank 
+simply focused on summarization, but could just as easily be used for
+keyphrase extraction or any other NLP ranking task. A more principled way to 
+estimate sentence importance is using random walks 
+and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
+to TextRank, and both use this approach for document summarization. The two 
+methods were developed by different groups at the same time, and LexRank 
+simply focused on summarization, but could just as easily be used for
+keyphrase extraction or any other NLP ranking task. A more principled way to 
+estimate sentence importance is using random walks 
+and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
+to TextRank, and both use this approach for document summarization. The two 
+methods were developed by different groups at the same time, and LexRank 
+simply focused on summarization, but could just as easily be used for
+keyphrase extraction or any other NLP ranking task. A more principled way to 
+estimate sentence importance is using random walks 
+and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
+to TextRank, and both use this approach for document summarization. The two 
+methods were developed by different groups at the same time, and LexRank 
+simply focused on summarization, but could just as easily be used for
+keyphrase extraction or any other NLP ranking task.simply focused on 
+summarization, but could just as easily be used for
+keyphrase extraction or any other NLP ranking task. A more principled way to 
+estimate sentence importance is using random walks 
+and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
+to TextRank, and both use this approach for document summarization. The two 
+methods were developed by different groups at the same time, and LexRank 
+simply focused on summarization, but could just as easily be used for
+keyphrase extraction or any other NLP ranking task. A more principled way to 
+estimate sentence importance is using random walks 
+and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
+to TextRank, and both use this approach for document summarization. The two 
+methods were developed by different groups at the same time, and LexRank 
+simply focused on summarization, but could just as easily be used for
+keyphrase extraction or any other NLP ranking task.";
+
+        $bigTextArray = explode(' ', $bigText);
+
+        //for ($i = 0; $i < 1500; $i++) {
+            $ngrams = $greeting->generateNgram($bigTextArray, 1);
+        //}
+
+        $this->assertEquals(530, count($ngrams));
     }
 }
