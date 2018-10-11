cd0b63800e934cc8e2aca4af5ba4a7f459342f18
Ehsan <iranianpep@gmail.com>
2018-10-12 08:21:40 +1100

    changes

--- src/KeywordExtractor/KeywordExtractor.php
+++ src/KeywordExtractor/KeywordExtractor.php
@@ -4,38 +4,76 @@ namespace KeywordExtractor;
 
 class KeywordExtractor
 {
-    public function generateNgram(array $tokens, $ngram = 2): array
-    {
-        $ngrams = [];
-        $length = count($tokens) - $ngram + 1;
-        for ($i = 0; $i < $length; $i++) {
-            $slice = array_slice($tokens, $i, $ngram);
+//    public function generateNgrams(array $tokens, $ngram = 2): array
+//    {
+//        $ngrams = [];
+//        $length = count($tokens) - $ngram + 1;
+//        for ($i = 0; $i < $length; $i++) {
+//            $slice = array_slice($tokens, $i, $ngram);
+//
+//            $ngrams[$i] = implode(' ', $slice);
+//        }
+//
+//        return $ngrams;
+//    }
 
-            $ngrams[$i] = implode(' ', $slice);
+    /**
+     * Generate n-grams
+     * Credits to https://github.com/yooper/php-text-analysis/blob/master/src/NGrams/NGramFactory.php
+     *
+     * @param array  $tokens
+     * @param        $ngramSize
+     * @param string $separator
+     *
+     * @return array
+     */
+    public function generateNgrams(array $tokens, $ngramSize, $separator = ' '): array
+    {
+        $separatorLength = strlen($separator);
+        $length = count($tokens) - $ngramSize + 1;
+
+        if ($length < 1) {
+            return [];
+        }
+
+        // initialize the array
+        $ngrams = array_fill(0, $length, '');
+
+        for ($i = 0; $i < $length; $i++) {
+            for ($j = 0; $j < $ngramSize; $j++) {
+                $ngrams[$i] .= $tokens[$i + $j];
+
+                if ($j < $ngramSize - $separatorLength) {
+                    $ngrams[$i] .= $separator;
+                }
+            }
         }
 
         return $ngrams;
     }
 
-    public function generateNgram2(array $tokens, $nGramSize, $separator = ' ') : array
+    /**
+     * @param $text
+     *
+     * @return string
+     */
+    public function removePunctuations($text): string
     {
-        $separatorLength = strlen($separator);
-        $length = count($tokens) - $nGramSize + 1;
-        if($length < 1) {
-            return [];
-        }
-        $ngrams = array_fill(0, $length, ''); // initialize the array
+        $searchFor = [
+            '!','#','$','%','&','\(','\)','*','+',"\'",',',
+            '\\','-','\.','\\/',':',';','<','=','>','?','@',
+            '^','_','`','{','|','}','~','\[','\]'
+        ];
 
-        for($index = 0; $index < $length; $index++)
-        {
-            for($jindex = 0; $jindex < $nGramSize; $jindex++)
-            {
-                $ngrams[$index] .= $tokens[$index + $jindex];
-                if($jindex < $nGramSize - $separatorLength) {
-                    $ngrams[$index] .= $separator;
-                }
-            }
-        }
-        return $ngrams;
+        $regex = "/([".implode("", $searchFor)."])/";
+
+//        (?!(\w)(.*)(\w))[.]
+//
+//
+//(\w{1})(\.{1})(\w{1})
+//
+//(\w\.\w)|(\w\.\w)
+        var_dump($regex);exit;
+        return preg_replace($regex, '', $text);
     }
 }
--- tests/KeywordExtractor/KeywordExtractorTest.php
+++ tests/KeywordExtractor/KeywordExtractorTest.php
@@ -8,89 +8,51 @@ class KeywordExtractorTest extends TestCase
 {
     public function testGenerateNgram()
     {
-        $greeting = new KeywordExtractor();
+        $keywordExtractor = new KeywordExtractor();
         $input = ['this', 'is', 'an', 'example'];
 
-        $ngrams = $greeting->generateNgram($input, 1);
+        $ngrams = $keywordExtractor->generateNgrams($input, 1);
         $this->assertEquals($input, $ngrams);
 
         $expected = ['this is', 'is an', 'an example'];
 
-        $ngrams = $greeting->generateNgram($input, 2);
+        $ngrams = $keywordExtractor->generateNgrams($input, 2);
         $this->assertEquals($expected, $ngrams);
 
         $expected = ['this is an', 'is an example'];
 
-        $ngrams = $greeting->generateNgram($input, 3);
+        $ngrams = $keywordExtractor->generateNgrams($input, 3);
         $this->assertEquals($expected, $ngrams);
 
         $expected = ['this is an example'];
 
-        $ngrams = $greeting->generateNgram($input, 4);
+        $ngrams = $keywordExtractor->generateNgrams($input, 4);
         $this->assertEquals($expected, $ngrams);
 
         $input = ['this', 'is'];
 
-        $ngrams = $greeting->generateNgram($input, 4);
+        $ngrams = $keywordExtractor->generateNgrams($input, 4);
         $this->assertEquals([], $ngrams);
+    }
+    
+    public function testRemovePunctuations()
+    {
+        $keywordExtractor = new KeywordExtractor();
+        $text = 'this is an example.';
 
-        $bigText = "A more principled way to estimate sentence importance is using random walks 
-and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
-to TextRank, and both use this approach for document summarization. The two 
-methods were developed by different groups at the same time, and LexRank 
-simply focused on summarization, but could just as easily be used for
-keyphrase extraction or any other NLP ranking task. A more principled way to 
-estimate sentence importance is using random walks 
-and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
-to TextRank, and both use this approach for document summarization. The two 
-methods were developed by different groups at the same time, and LexRank 
-simply focused on summarization, but could just as easily be used for
-keyphrase extraction or any other NLP ranking task. A more principled way to 
-estimate sentence importance is using random walks 
-and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
-to TextRank, and both use this approach for document summarization. The two 
-methods were developed by different groups at the same time, and LexRank 
-simply focused on summarization, but could just as easily be used for
-keyphrase extraction or any other NLP ranking task. A more principled way to 
-estimate sentence importance is using random walks 
-and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
-to TextRank, and both use this approach for document summarization. The two 
-methods were developed by different groups at the same time, and LexRank 
-simply focused on summarization, but could just as easily be used for
-keyphrase extraction or any other NLP ranking task. A more principled way to 
-estimate sentence importance is using random walks 
-and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
-to TextRank, and both use this approach for document summarization. The two 
-methods were developed by different groups at the same time, and LexRank 
-simply focused on summarization, but could just as easily be used for
-keyphrase extraction or any other NLP ranking task. A more principled way to 
-estimate sentence importance is using random walks 
-and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
-to TextRank, and both use this approach for document summarization. The two 
-methods were developed by different groups at the same time, and LexRank 
-simply focused on summarization, but could just as easily be used for
-keyphrase extraction or any other NLP ranking task.simply focused on 
-summarization, but could just as easily be used for
-keyphrase extraction or any other NLP ranking task. A more principled way to 
-estimate sentence importance is using random walks 
-and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
-to TextRank, and both use this approach for document summarization. The two 
-methods were developed by different groups at the same time, and LexRank 
-simply focused on summarization, but could just as easily be used for
-keyphrase extraction or any other NLP ranking task. A more principled way to 
-estimate sentence importance is using random walks 
-and eigenvector centrality. LexRank[5] is an algorithm essentially identical 
-to TextRank, and both use this approach for document summarization. The two 
-methods were developed by different groups at the same time, and LexRank 
-simply focused on summarization, but could just as easily be used for
-keyphrase extraction or any other NLP ranking task.";
+        $result = $keywordExtractor->removePunctuations($text);
+        $this->assertEquals('this is an example', $result);
 
-        $bigTextArray = explode(' ', $bigText);
+        $keywordExtractor = new KeywordExtractor();
+        $text = 'this is, an example.';
 
-        //for ($i = 0; $i < 1500; $i++) {
-            $ngrams = $greeting->generateNgram($bigTextArray, 1);
-        //}
+        $result = $keywordExtractor->removePunctuations($text);
+        $this->assertEquals('this is an example', $result);
 
-        $this->assertEquals(530, count($ngrams));
+        $keywordExtractor = new KeywordExtractor();
+        $text = 'this is a text containing node.js.';
+
+        $result = $keywordExtractor->removePunctuations($text);
+        $this->assertEquals('this is a text containing nodejs', $result);
     }
 }
