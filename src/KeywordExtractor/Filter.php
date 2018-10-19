ac7a4f5ae4578ee5430b841ae18bc140f1753145
Ehsan Abbasi <iranianpep@gmail.com>
2018-10-19 12:26:10 +1100

    fix handling keywords like c#

--- src/KeywordExtractor/Filter.php
+++ src/KeywordExtractor/Filter.php
@@ -75,10 +75,14 @@ class Filter
     public function removeEmails($string):? string
     {
         // break the pattern to avoid exceeding the line
-        $pattern = '/(?:[A-Za-z0-9!#$%&\'*+=?^_`{|}~-]+(?:\.[A-Za-z0-9!#$%&\'*+=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]';
-        $pattern .= '|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?';
-        $pattern .= '|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?';
-        $pattern .= '|[A-Za-z0-9-]*[A-Za-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/';
+        $pattern = '/(?:[A-Za-z0-9!#$%&\'*+=?^_`{|}~-]+(?:\.[A-Za-z0-9!#$%&\'*+=?^_`{|}~-]+)*';
+        $pattern .= '|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]';
+        $pattern .= '|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[A-Za-z0-9]';
+        $pattern .= '(?:[A-Za-z0-9-]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?';
+        $pattern .= '|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}';
+        $pattern .= '(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?';
+        $pattern .= '|[A-Za-z0-9-]*[A-Za-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]';
+        $pattern .= '|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/';
 
         return preg_replace($pattern, '', $string);
     }
