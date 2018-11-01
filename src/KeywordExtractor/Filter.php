21f87f4e198c36b01adb36b7a1e0db7b75cc71c2
Ehsan <iranianpep@gmail.com>
2018-11-01 21:01:36 +1100

    changes

--- src/KeywordExtractor/Filter.php
+++ src/KeywordExtractor/Filter.php
@@ -5,58 +5,6 @@ namespace KeywordExtractor;
 class Filter
 {
     /**
-     * @param $word
-     *
-     * @return string
-     */
-    public function removeRightPunctuation($word): string
-    {
-        $searchFor = [
-            '!', '$', '%', '&', '(', ')', '*', "'", ',',
-            '\\', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@',
-            '^', '_', '`', '{', '|', '}', '~', '[', ']',
-        ];
-
-        return rtrim($word, " \t\n\r\0\x0B".implode('', $searchFor));
-    }
-
-    /**
-     * @param array $words
-     * @param array $whitelist
-     *
-     * @return array
-     */
-    public function removeRightPunctuations(array $words, array $whitelist = []): array
-    {
-        foreach ($words as $key => $word) {
-            if (in_array($word, $whitelist)) {
-                continue;
-            }
-
-            $words[$key] = $this->removeRightPunctuation($word);
-        }
-
-        return $words;
-    }
-
-    /**
-     * @param $words
-     *
-     * @return array
-     */
-    public function removeNumbers(array $words): array
-    {
-        foreach ($words as $key => $word) {
-            if (is_numeric($word) === true) {
-                unset($words[$key]);
-            }
-        }
-
-        // re-index the array
-        return array_values($words);
-    }
-
-    /**
      * @param $words
      * @param $indexes
      *
@@ -69,26 +17,6 @@ class Filter
         }
 
         return $words;
-    }
-
-    /*
-     * @param $string
-     *
-     * @return null|string
-     */
-    public function removeEmails($string):? string
-    {
-        // break the pattern to avoid exceeding the line
-        $pattern = '/(?:[A-Za-z0-9!#$%&\'*+=?^_`{|}~-]+(?:\.[A-Za-z0-9!#$%&\'*+=?^_`{|}~-]+)*';
-        $pattern .= '|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]';
-        $pattern .= '|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[A-Za-z0-9]';
-        $pattern .= '(?:[A-Za-z0-9-]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[A-Za-z0-9-]*[A-Za-z0-9])?';
-        $pattern .= '|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}';
-        $pattern .= '(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?';
-        $pattern .= '|[A-Za-z0-9-]*[A-Za-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]';
-        $pattern .= '|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/';
-
-        return preg_replace($pattern, '', $string);
     }
 
     /**
--- tests/KeywordExtractor/FilterTest.php
+++ tests/KeywordExtractor/FilterTest.php
@@ -6,62 +6,6 @@ use PHPUnit\Framework\TestCase;
 
 class FilterTest extends TestCase
 {
-    public function testRemoveRightPunctuations()
-    {
-        $filter = new Filter();
-
-        $inputsOutputs = [
-            [
-                'i' => ['test.', '.test', '.', '.test.', '', 'node.js?', 'node.js???', 'c#'],
-                'o' => ['test', '.test', '', '.test', '', 'node.js', 'node.js', 'c#'],
-            ],
-            [
-                'i' => [],
-                'o' => [],
-            ],
-            [
-                'i' => ['0', 0, '', 1],
-                'o' => ['0', 0, '', 1],
-            ],
-            [
-                'i' => ['visual studio 2018', 'knockout.js', '- knockout...js?'],
-                'o' => ['visual studio 2018', 'knockout.js', '- knockout...js'],
-            ],
-        ];
-
-        foreach ($inputsOutputs as $inputOutput) {
-            $this->assertEquals($inputOutput['o'], $filter->removeRightPunctuations($inputOutput['i']));
-        }
-    }
-
-    public function testRemoveNumbers()
-    {
-        $filter = new Filter();
-
-        $inputsOutputs = [
-            [
-                'i' => ['1'],
-                'o' => [],
-            ],
-            [
-                'i' => [1],
-                'o' => [],
-            ],
-            [
-                'i' => [1, 2, '1', '0', 0, '', 'test1', '1test', 'test1test', 'c#'],
-                'o' => ['', 'test1', '1test', 'test1test', 'c#'],
-            ],
-            [
-                'i' => [],
-                'o' => [],
-            ],
-        ];
-
-        foreach ($inputsOutputs as $inputOutput) {
-            $this->assertEquals($inputOutput['o'], $filter->removeNumbers($inputOutput['i']));
-        }
-    }
-
     public function testRemoveWordsByIndexes()
     {
         $filter = new Filter();
@@ -102,34 +46,6 @@ class FilterTest extends TestCase
                 $inputOutput['o'],
                 $filter->removeWordsByIndexes($inputOutput['i']['words'], $inputOutput['i']['indexes'])
             );
-        }
-    }
-
-    public function testRemoveEmails()
-    {
-        $filter = new Filter();
-
-        $inputsOutputs = [
-            [
-                'i' => 'test@example.com.',
-                'o' => '.',
-            ],
-            [
-                'i' => 'this contains an email e.g. test@example.com.',
-                'o' => 'this contains an email e.g. .',
-            ],
-            [
-                'i' => 'this contains an email e.g. invalid@email.',
-                'o' => 'this contains an email e.g. invalid@email.',
-            ],
-            [
-                'i' => 'this contains two emails valid@gmail.com and valid2@gmail.com',
-                'o' => 'this contains two emails  and ',
-            ],
-        ];
-
-        foreach ($inputsOutputs as $inputOutput) {
-            $this->assertEquals($inputOutput['o'], $filter->removeEmails($inputOutput['i']));
         }
     }
 
