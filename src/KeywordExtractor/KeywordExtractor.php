1e8e77688ee38db3c235b54d685dbf26a394441c
Ehsan Abbasi <iranianpep@gmail.com>
2018-11-04 12:39:14 +0000

    Apply fixes from StyleCI

--- src/KeywordExtractor/KeywordExtractor.php
+++ src/KeywordExtractor/KeywordExtractor.php
@@ -162,7 +162,7 @@ class KeywordExtractor
         $alreadyAdded = false;
 
         /**
-         * @var $modifier ModifierInterface
+         * @var ModifierInterface
          */
         foreach ($this->getModifiers() as $modifier) {
             $toBeModified = $word;
@@ -300,7 +300,7 @@ class KeywordExtractor
 
         $this->keywords[$keyword] = [
             'frequency' => $frequency,
-            'originals' => $originals
+            'originals' => $originals,
         ];
     }
 
