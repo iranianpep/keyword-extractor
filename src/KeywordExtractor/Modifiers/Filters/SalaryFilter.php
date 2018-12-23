21aae5d582e3f172ee21e4bcd628f29184e4d614
Ehsan <iranianpep@gmail.com>
2018-12-23 21:57:37 +1100

    update salary regex

--- src/KeywordExtractor/Modifiers/Filters/SalaryFilter.php
+++ src/KeywordExtractor/Modifiers/Filters/SalaryFilter.php
@@ -8,7 +8,7 @@ class SalaryFilter extends AbstractModifier
 {
     public function modifyToken($token)
     {
-        if (preg_match('/^\$?[0-9]*[,]*[0-9]*(\.[0-9]*)?(k|(\/)*hr|(\/)*hour)*$/', $token)) {
+        if (preg_match('/^\$?[0-9]*[,]*[0-9]*(\.[0-9]*)?(k|(\/)*hr|(\/)*hour|(\/)*ph|(\/)*b|(\/)*m|(\/)*day)*$/', $token)) {
             return '';
         }
 
