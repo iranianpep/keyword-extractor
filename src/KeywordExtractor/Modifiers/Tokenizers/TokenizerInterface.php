b1d62ad9b90998557d255039ee01a6858019370b
Sergey Kudashev <kudashevs@gmail.com>
2024-12-21 15:46:58 +0300

    Update Tokenizers add a Tokenizer interface

---
+++ src/KeywordExtractor/Modifiers/Tokenizers/TokenizerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace KeywordExtractor\Modifiers\Tokenizers;
+
+interface TokenizerInterface
+{
+    /**
+     * Break a character sequence to a sequence of tokens.
+     *
+     * @param string $str Text to be tokenized
+     * @return array  List of tokens from the string
+     */
+    public function tokenize($str): array;
+}
